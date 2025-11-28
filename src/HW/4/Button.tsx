9060e47d4952369b972215e6977bdba99d394625
StasBolotov <Stashek.web.404@gmail.com>
2025-11-28 12:49:54 +0300

    dz-4

--- src/HW/4/Button.tsx
+++ src/HW/4/Button.tsx
@@ -1,16 +1,19 @@
+import { MouseEvent } from 'react';
+
 type ButtonPropsType = {
-  callBack: any // НУЖНО ПРОТИПИЗИРОВАТЬ
-  name: any // НУЖНО ПРОТИПИЗИРОВАТЬ
+  callBack: () => void;
+  name: string;
 };
 
 export const Button = (props: ButtonPropsType) => {
-  const callBackHandler = () => {
-    // НУЖНО ДОПИСАТЬ
+  const callBackHandler = (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    props.callBack();
   };
 
   return (
-    <button id={'hw04-button'} onClick={callBackHandler}>
-      {props.name}
-    </button>
+      <button id={'hw04-button'} onClick={callBackHandler}>
+        {props.name}
+      </button>
   );
 };
